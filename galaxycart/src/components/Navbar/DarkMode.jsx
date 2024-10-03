a1ad3e11aefdac455a1393ecd4c5c52c0c3a19bb
R2sipho <r2sipho@gmail.com>
2024-10-03 09:31:21 +0200

    added images and a toggle button for theme

--- galaxycart/src/components/Navbar/DarkMode.jsx
+++ galaxycart/src/components/Navbar/DarkMode.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React from "react";
+import LightButton from "../../assets/website/light-mode-button.png";
+import DarkButton from "../../assets/website/dark-mode-button.png";
 
-const DarkMode = () =>{
-  return <div>DarkMode</div>;
-  
+const DarkMode = () => {
+  const [theme, setTheme] = React.useState("light");
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+  <div>
+    <img 
+        src={theme === "light" ? LightButton : DarkButton} 
+        alt="Toggle Theme"
+        className="w-12 cursor-pointer" 
+        onClick={toggleTheme} 
+       />
+  </div>
+  );
 };
 
 export default DarkMode;
