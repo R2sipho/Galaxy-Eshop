ad3b44d8afff1f635b056674cd0256413e8c3fc6
R2sipho <r2sipho@gmail.com>
2024-10-07 19:20:51 +0200

    buy now button

--- galaxycart/src/components/Hero/Hero.jsx
+++ galaxycart/src/components/Hero/Hero.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Slider from "react-slick";
+import Headphone from "../../assets/hero/headphone.png";
+import Watch from "../../assets/hero/watch.png"; 
+import Macbook from "../../assets/category/macbook.png"; 
+
 
 const HeroData = [
     {
         id: 1,
-        img: Image1,
+        img: Headphone,
         subtitle: "Beats solo",
         title: "wireless",
         title2: "Headphone",
@@ -12,20 +16,21 @@ const HeroData = [
     },
     {
         id: 2,
-        img: Image2,
+        img: Watch,
         subtitle: "Beats solo",
-        title: "wireless",
+        title: "Watch",
         title2: "Virtual",
         description:"",
     },
     {
         id: 3,
-        img: Image1,
+        img: Macbook,
         subtitle: "Beats solo",
-        title: "wireless",
-        title2: "Headphone",
+        title: "Watch",
+        title2: "Virtual",
         description:"",
     },
+    
 ]
 
 const Hero =() => {
@@ -41,15 +46,40 @@ const Hero =() => {
         pauseOnFocus: true,
     };
     return (
-    <div>
-        <div className="w-full">
-            {/**Hero Section */}
-            <Slider {...settings} > 
-           
-            </Slider>
+        <div className="container">
+          <div className="overflow-hidden rounded-3xl min-h-[550px] sm:min-h-[650px] hero-bg-color flex justify-center items-center">
+            <div className="container pb-8 sm:pb-0">
+              {/* Hero Section */}
+              <Slider {...settings}>
+                {HeroData.map((data) => (
+                  <div key={data.id}>
+                    <div className="grid grid-cols-1 sm:grid-cols-2">
+                      {/* Text content section */}
+                      <div>
+                        <h1>{data.subtitle}</h1>
+                        <h1>{data.title}</h1>
+                        <h1>{data.title2}</h1>
+                        <div>
+                          <button>Shop Now</button>
+                        </div>
+                      </div>
+                      {/* Image section */}
+                      <div>
+                        <div>
+                          <img
+                            src={data.img}
+                            alt=""
+                            className="w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
         </div>
-    </div>
-    );
-};
-
+      );
+    };
 export default Hero;
