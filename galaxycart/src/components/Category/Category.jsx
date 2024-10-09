45c6483d53c74bd11b29630071eb0908c178fd81
R2sipho <r2sipho@gmail.com>
2024-10-09 17:25:30 +0200

    adding grid

---
+++ galaxycart/src/components/Category/Category.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image1 from '../../assets/Category/earphone.png';
+import Image2 from '../../assets/Category/watch.png';
+import Image3 from '../../assets/Category/macbook.png';
+
+const Category = () => {
+  return (
+        <div className='py-8'>
+            <div className='container'>
+                <div className='grid'>
+                    {/** */}
+
+                </div>
+            </div>
+
+        </div>
+  );
+};
+
+export default Category;
--- galaxycart/src/components/Hero/Hero.jsx
+++ galaxycart/src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import Headphone from "../../assets/hero/headphone.png";
 import Watch from "../../assets/hero/watch.png"; 
 import Macbook from "../../assets/category/macbook.png"; 
+import Button from '../Shared/Button';
 
 
 const HeroData = [
@@ -55,21 +56,27 @@ const Hero =() => {
                   <div key={data.id}>
                     <div className="grid grid-cols-1 sm:grid-cols-2">
                       {/* Text content section */}
-                      <div>
-                        <h1>{data.subtitle}</h1>
-                        <h1>{data.title}</h1>
-                        <h1>{data.title2}</h1>
+                      <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                        <h1 className="text-2xl sm:text-6xl lg:text-2xl font-bold">{data.subtitle}</h1>
+                        <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">{data.title}</h1>
+                        <h1 className="text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold">{data.title2}</h1>
                         <div>
-                          <button>Shop Now</button>
+                        <Button 
+                           text="Shop Now" 
+                           bgColor="bg-primary" 
+                           textColor="text-white"   
+                        />
+                           
+                          
                         </div>
                       </div>
                       {/* Image section */}
-                      <div>
+                      <div className="order-1 sm:order-2">
                         <div>
                           <img
                             src={data.img}
                             alt=""
-                            className="w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]"
+                            className="w-[300px] sm:w-[450] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40"
                           />
                         </div>
                       </div>
