2d310a8acaa3cfbd8774c2141decd4ccdc3cebd4
R2sipho <r2sipho@gmail.com>
2024-10-09 18:29:54 +0200

    adding images to colum

--- galaxycart/src/components/Category/Category.jsx
+++ galaxycart/src/components/Category/Category.jsx
@@ -10,12 +10,12 @@ const Category = () => {
             <div className='container'>
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
                     {/**First col */}
-                    <div>
+                    <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end'>
                         <div>
-                            <div>
-                                <p>Enjoy</p>
-                                <p>With</p>
-                                <p>Earphone</p>
+                            <div className='mb-4'>
+                                <p className='mb-[2px] text-gray-400'>Enjoy</p>
+                                <p className='text-2xl font-semibold mb-[2px]'>With</p>
+                                <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Earphone</p>
                                 <Button 
                                     text="Browse"
                                     bgColor={"bg-primary"}
@@ -23,9 +23,26 @@ const Category = () => {
                                 />
                             </div>
                         </div>
+                        <img src={Image1} alt='' className='w-[320px] absolute bottom-0' />
 
                     </div>
                     {/**Second col */}
+                    <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end'>
+                        <div>
+                            <div className='mb-4'>
+                                <p className='mb-[2px] text-gray-400'>Enjoy</p>
+                                <p className='text-2xl font-semibold mb-[2px]'>With</p>
+                                <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Gadget</p>
+                                <Button 
+                                    text="Browse"
+                                    bgColor={"bg-primary"}
+                                    textColor={"text-white"}
+                                />
+                            </div>
+                        </div>
+                        <img src={Image2} alt='' className='w-[320px] absolute -right-4 lg:top-[40px]' />
+
+                    </div>
                     {/**third col */}
 
                 </div>
