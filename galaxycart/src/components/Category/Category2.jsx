3f6ac7d199c1bb28b2c2dafed16f487138358500
R2sipho <r2sipho@gmail.com>
2024-10-13 18:04:07 +0200

    creating services

--- galaxycart/src/components/Category/Category2.jsx
+++ galaxycart/src/components/Category/Category2.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Image1 from '../../assets/Category/earphone.png';
-import Image2 from '../../assets/Category/watch.png';
-import Image3 from '../../assets/Category/macbook.png';
+import Image1 from '../../assets/Category/gaming.png';
+import Image2 from '../../assets/Category/vr.png';
+import Image3 from '../../assets/Category/speaker.png';
 import Button from '../Shared/Button';
 
 const Category = () => {
@@ -10,12 +10,12 @@ const Category = () => {
             <div className='container'>
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
                     {/**First col */}
-                    <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end'>
+                    <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-green-800 text-white rounded-3xl relative h-[320px] flex items-start'>
                         <div>
                             <div className='mb-4'>
                                 <p className='mb-[2px] text-gray-400'>Enjoy</p>
                                 <p className='text-2xl font-semibold mb-[2px]'>With</p>
-                                <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Earphone</p>
+                                <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>VR</p>
                                 <Button 
                                     text="Browse"
                                     bgColor={"bg-primary"}
@@ -23,16 +23,16 @@ const Category = () => {
                                 />
                             </div>
                         </div>
-                        <img src={Image1} alt='' className='w-[320px] absolute bottom-0' />
+                        <img src={Image2} alt='' className='w-[320px] absolute bottom-0' />
 
                     </div>
                     {/**Second col */}
-                    <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end'>
+                    <div className='py-10 pl-5 bg-gradient-to-br from-blue-800/90 to-blue-950/90 text-white rounded-3xl relative h-[320px] flex items-start'>
                         <div>
                             <div className='mb-4'>
                                 <p className='mb-[2px] text-gray-400'>Enjoy</p>
                                 <p className='text-2xl font-semibold mb-[2px]'>With</p>
-                                <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Gadget</p>
+                                <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Speaker</p>
                                 <Button 
                                     text="Browse"
                                     bgColor={"bg-primary"}
@@ -40,24 +40,24 @@ const Category = () => {
                                 />
                             </div>
                         </div>
-                        <img src={Image2} alt='' className='w-[320px] absolute -right-4 lg:top-[40px]' />
+                        <img src={Image3} alt='' className='w-[200px] absolute bottom-0 right-0' />
 
                     </div>
                     {/**third col */}
-                    <div className='col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end'>
+                    <div className='col-span-2 py-10 pl-5 bg-gradient-to-br from-gray-400/90 to-gray-100 text-white rounded-3xl relative h-[320px] flex items-start'>
                         <div>
                             <div className='mb-4'>
                                 <p className='mb-[2px] text-white'>Enjoy</p>
                                 <p className='text-2xl font-semibold mb-[2px]'>With</p>
-                                <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Laptop</p>
+                                <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>PlayStation</p>
                                 <Button 
                                     text="Browse"
-                                    bgColor={"bg-white"}
-                                    textColor={"text-primary"}
+                                    bgColor={"bg-primary"}
+                                    textColor={"text-white"}
                                 />
                             </div>
                         </div>
-                        <img src={Image3} alt='' className='w-[250px] absolute top-1/2 -translate-y-1/2 -right-0' />
+                        <img src={Image1} alt='' className='w-[250px] absolute top-1/2 -translate-y-1/2 -right-0' />
 
                     </div>
 
